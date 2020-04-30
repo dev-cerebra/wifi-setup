94c5a7a3ea554fb968b4e477d4c6205be8ab3dda
Renan Fazzani <renan.alexandre@cerebrasc.com.br>
2020-04-30 14:22:28 -0300

    Update captive_portal.js

--- fs/captive_portal.js
+++ fs/captive_portal.js
@@ -82,10 +82,10 @@
                 var stringifyJson = JSON.stringify(jsonResponse, undefined, 2);
                 responseVal = highlight(stringifyJson);
                 //dados obtidos do .json via comando Config.Get, possui todos dados de configs disponíveis
-            document.getElementById("response").innerHTML = "Station:<br>" + "ssid: " + resp.wifi.sta.ssid 
-            + "<br>" + "pass: " + resp.wifi.sta.pass + "<br>" + "<br>" + "Access Point:<br>" 
-            + "ssid: " + resp.wifi.ap.ssid + "<br>" + "pass: " + resp.wifi.ap.pass + "<br>" + "Device: "
-             + resp.wifi.ap.ip + "<br>" + "Device: " + resp.device.id; 
+            document.getElementById("response").innerHTML = "Device: " + resp.device.id + "<br>"
+            "Station:<br>" + "ssid: " + resp.wifi.sta.ssid + "<br>" + "pass: " + resp.wifi.sta.pass
+             + "<br>" + "<br>" + "Access Point:<br>" + "ssid: " + resp.wifi.ap.ssid + "<br>" 
+             + "pass: " + resp.wifi.ap.pass + "<br>" + "ip: " + resp.wifi.ap.ip; 
             } else {
                 responseVal = "Unable to get info from device!";
                 document.getElementById("response").innerHTML = responseVal ? responseVal : '';
