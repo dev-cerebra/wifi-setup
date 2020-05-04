7990ccdbfd3a9d507472f3bec115f2e84fed53b2
Renan Fazzani <renan.alexandre@cerebrasc.com.br>
2020-05-04 14:20:25 -0300

    Update captive_portal.js

--- fs/captive_portal.js
+++ fs/captive_portal.js
@@ -85,7 +85,8 @@
                 //dados obtidos do .json via comando Config.Get, possui todos dados de configs disponíveis
             document.getElementById("response").innerHTML = "Station:<br>" + "ssid: " + resp.wifi.sta.ssid 
             + "<br>" + "pass: " + resp.wifi.sta.pass + "<br>" + "<br>" + "Access Point:<br>" 
-            + "ssid: " + resp.wifi.ap.ssid + "<br>" + "pass: " + resp.wifi.ap.pass + "<br>" + "ip: " + resp.wifi.ap.ip; 
+            + "ssid: " + resp.wifi.ap.ssid + "<br>" + "pass: " + resp.wifi.ap.pass + "<br>" + "ip: " + resp.wifi.ap.ip 
+            + "<br>" + "Device: " + resp.device.id; 
             } else {
                 responseVal = "Unable to get info from device!";
                 document.getElementById("response").innerHTML = responseVal ? responseVal : '';
